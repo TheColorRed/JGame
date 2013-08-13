80b48464c3e9276278b507d80386c45c747b74fd
ryan <ryan@ryan-Inspiron-M5030>
2013-08-13 00:12:00 -0400

    Added Create GameObject at a radom location

--- src/JGame/Actions/CreateAction.java
+++ src/JGame/Actions/CreateAction.java
@@ -3,6 +3,7 @@ package JGame.Actions;
 import JGame.GameObject.GameObject;
 import JGame.GameObject.Sprite;
 import JGame.Room.Room;
+import JGame.Util.Math2;
 
 public class CreateAction extends Action{
 
@@ -13,6 +14,16 @@ public class CreateAction extends Action{
 
     public GameObject createAt(String reference, Sprite sprite, Room room, int x, int y){
         GameObject obj = new GameObject(reference, sprite, room);
+        room.addGameObjectAt(obj, x, y);
+        return obj;
+    }
+
+    public GameObject createAtRandom(String reference, Sprite sprite, Room room){
+        GameObject obj = new GameObject(reference, sprite, room);
+        int maxX = room.getWidth() - obj.getWidth();
+        int maxY = room.getHeight() - obj.getHeight();
+        int x = Math2.random(0, maxX);
+        int y = Math2.random(0, maxY);
         room.addGameObjectAt(obj, x, y);
         return obj;
     }
